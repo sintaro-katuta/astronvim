bf2f3b3828132ee743b337cbd70c3dfd21dad321
sintaro-katuta <sintaro.katuta@gmail.com>
2025-02-18 02:08:45 +0900

    lazygit colorschemaを登録

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -123,4 +123,38 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' }
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+    {
+    'lmantw/themify.nvim',
+      
+    lazy = false,
+    priority = 999,
+
+    config = {}
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+}
 }
